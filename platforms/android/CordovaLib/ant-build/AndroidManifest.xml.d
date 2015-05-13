404f17ae53e7f5135ad370bf0cb4d21a046fda1e
Laura17 <laura.gutierrez@kmedia.co>
2015-05-13 10:58:52 -0500

    Peso2

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\vxadmin\Documents\MP\KMEDIA\FUCS\fucsapp\platforms\android\CordovaLib\ant-build\AndroidManifest.xml : \
-C:\Users\vxadmin\Documents\MP\KMEDIA\FUCS\fucsapp\platforms\android\CordovaLib\AndroidManifest.xml \
--- platforms/android/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\vxadmin\Documents\MP\KMEDIA\FUCS\fucsapp\platforms\android\ant-build\AndroidManifest.xml : \
-C:\Users\vxadmin\Documents\MP\KMEDIA\FUCS\fucsapp\platforms\android\AndroidManifest.xml \
